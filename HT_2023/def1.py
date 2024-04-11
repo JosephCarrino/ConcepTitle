2c777e3d8e3c351d6f7858216743655663921013
romanellas <www.daniele.romanella@gmail.com>
2024-04-11 17:22:29 +0200

    Fix

--- HT_2023/def1.py
+++ HT_2023/def1.py
@@ -1,4 +1,7 @@
 # 1. Given a news, who skipped it?
+# 1. Data una notizia totalmente contenuta nel file indicato da news_snap
+#    Quale giornale, di quelli dati riporta la stessa notizia?
+
 
 import json
 import random
@@ -8,10 +11,11 @@ from typing import List
 warnings.filterwarnings("ignore")
 
 snap_dir = "ANSA_Politica"
-news_snap = "2022-05-11T17.34.30E1652290470.188478.json"
-main_dir = "../fulltext/NER/flow/IT"
+news_snap = "ner_en_2024-04-05T10.00.23E1712311223.224481.json"
+main_dir = "../../Newscraping/collectedNews/flow/IT"
 
 skip_dirs = ["AGI_Politica"]
+SENTIMENT_ANALYSIS = False
 
 to_check_dir = f"{main_dir}/{snap_dir}/{news_snap}"
 
@@ -22,9 +26,9 @@ def main():
 def skipped_by(to_check: dict) -> List[str]:
     skipping_sources = []
     for skip_dir in skip_dirs:
-        snapped = snapped_news_by_source(f"{main_dir}/{skip_dir}")
+        snapped = snapped_news_by_source(f"{main_dir}/{skip_dir}", SENTIMENT_ANALYSIS)
         has_similar = has_similar_in_snapshot(to_check, snapped)
-        if not has_similar:
+        if not has_similar: # Perché qui c'è un not?
             skipping_sources.append(skip_dir)
     print(f"Sources which skipped the given news: {skipping_sources}")
     return skipping_sources
