77e5c117b0cceb454849e66abe9f8bf49bd35c47
daniele.romanella02@gmail.com <www.daniele.romanella@gmail.com>
2024-04-17 20:11:52 +0200

    AV

--- cronjob.sh
+++ cronjob.sh
@@ -1,5 +1,5 @@
 source /home/romanellas/venv/bin/activate &&
 cd /mediam/mc/Thesis/ConcepTitle/pipeline &&
-python3 translator.py &&
-python3 NER_recognizer.py &&
+# python3 translator.py &&
+# python3 NER_recognizer.py &&
 python3 NLPer.py
