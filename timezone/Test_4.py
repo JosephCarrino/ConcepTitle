75e3eb3bc8a94d96500a467d7d2c005bb1457009
romanellas <www.daniele.romanella@gmail.com>
2024-05-01 21:58:16 +0200

    Avanzamento

--- timezone/Test_4.py
+++ timezone/Test_4.py
@@ -3,7 +3,7 @@
 import multiprocessing
 from datetime import datetime
 from multiprocessing import Process
-from utils import extract_articles, calculate_nlp, array_nlp
+from utils import extract_articles, calculate_nlp, array_nlp, calculate_similarity
 
 # python3 -m spacy download en_core_web_trf
 
@@ -34,6 +34,9 @@ def main():
     articles = []
     articles_new = []
     processes = []
+    articles_nro = []
+    articles_not_unique_title = []
+
     for (label, path) in enumerate(NEWS_PAPERS):
         tmp = Process(target=extract_articles, args=(BASE_DIR, path, DAY, START_TIME, END_TIME, queue, label))
         processes.append(tmp)
@@ -54,6 +57,8 @@ def main():
     for (index, article_set) in enumerate(articles):
         tmp = Process(target=array_nlp, args=(article_set, index, queue))
         processes.append(tmp)
+        articles_nro.append(len(article_set))
+        articles_not_unique_title.append([])
 
     for p in processes:
         p.start()
@@ -63,40 +68,23 @@ def main():
         articles[label] = articles_found
 
     print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+
+    for (label_a, path_a) in enumerate(NEWS_PAPERS):
+        for (label_b, path_b) in enumerate(NEWS_PAPERS):
+            if label_a == label_b:
+                continue
+
+            for article_a in articles[label_a]:
+                for article_b in articles[label_b]:
+                    (similarity, percentage) = calculate_similarity(article_a["cont_nlp"], article_b["cont_nlp"],
+                                                                    COSINE_THRESHOLD)
+                    if similarity:
+                        if article_a["en_title"] not in articles_not_unique_title[label_a]:
+                            articles_not_unique_title[label_a].append(article_a["en_title"])
+                        if article_b["en_title"] not in articles_not_unique_title[label_b]:
+                            articles_not_unique_title[label_b].append(article_b["en_title"])
+    print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
     articles = []
-
-    #TODO: CONTINUE HERE
-
-    """
-    for article in articles_a:
-        article = calculate_nlp(article)
-        new_a.append(article)
-
-    del articles_a
-
-    for article in articles_b:
-        article = calculate_nlp(article)
-        new_b.append(article)
-
-    del articles_b
-
-    simils = []
-    for article_a in new_a:
-        for article_b in new_b:
-            (similarity, percentage) = calculate_similarity(article_a["cont_nlp"], article_b["cont_nlp"],
-                                                            COSINE_THRESHOLD)
-            if similarity:
-                simils.append((article_a, article_b, percentage))
-
-    for simil_triple in simils:
-        at = simil_triple[0]["title"]
-        an = simil_triple[0]["news_url"]
-        bt = simil_triple[1]["title"]
-        bn = simil_triple[1]["news_url"]
-        print(f"{at} - {an} - {bt} - {bn} - {simil_triple[2]}")
-    
-    """
-
 
 if __name__ == '__main__':
     main()
